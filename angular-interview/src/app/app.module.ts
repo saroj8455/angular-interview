688563dba1560a64b0445e98da3bc4f5c2766c5f
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-07-26 23:07:09 +0530

    refactor app component and move to landing component

--- angular-interview/src/app/app.module.ts
+++ angular-interview/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
